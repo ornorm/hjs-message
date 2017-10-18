7c2a45f554da899d5d23d58e4bb18ab8a72f56e4
biendo <aime@awelty.com>
2017-10-18 16:00:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Product Name
+> Messaging API of the Hubrisjs javascript framework.
+
+Messaging classes: MessageBroadcaster, MessageHandler, MessageQueue and CountDownTimer.
+
+## Installation
+
+Node:
+
+```sh
+npm install hjs-message --save
+```
+
+## Meta
+
+Aime – abiendo@gmail.com
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
---
+++ index.js
@@ -0,0 +1,43 @@
+/** @babel */
+import {
+    Registration,
+    MessageBroadcaster} from './lib/broadcaster';
+import {
+    Callback,
+    Runnable,
+    BlockingRunnable,
+    Messenger,
+    MessageHandler} from './lib/handler';
+import {
+    FLAG_IN_USE,
+    FLAG_ASYNCHRONOUS,
+    FLAGS_TO_CLEAR_ON_COPY_FROM,
+    Message} from './lib/message';
+import {
+    IdleHandler,
+    MessageQueue} from './lib/queue';
+import {
+    CountDownTimer} from './lib/timer';
+
+export {
+    Registration,
+    MessageBroadcaster,
+
+    Callback,
+    Runnable,
+    BlockingRunnable,
+    Messenger,
+    MessageHandler,
+
+    FLAG_IN_USE,
+    FLAG_ASYNCHRONOUS,
+    FLAGS_TO_CLEAR_ON_COPY_FROM,
+    Message,
+
+    IdleHandler,
+    MessageQueue,
+
+    CountDownTimer
+}
+
+
